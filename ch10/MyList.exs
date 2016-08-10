9a3a483fed14052e65940bdb86efd6a99c822447
wangchi <wangchi@meican.com>
2016-08-10 15:57:18 +0800

    optimize ch10

--- ch10/MyList.exs
+++ ch10/MyList.exs
@@ -4,6 +4,6 @@ defmodule MyList do
   end
 
   def _flatten([], acc), do: acc
-  def _flatten([head | tail], acc) when is_list(head), do: _flatten(head, acc) ++ _flatten(tail, acc) ++ acc
-  def _flatten([head | tail], acc), do: [head] ++ _flatten(tail, acc) ++ acc
+  def _flatten([head | tail], acc) when is_list(head), do: _flatten(head, _flatten(tail, acc))
+  def _flatten([head | tail], acc), do: [head | _flatten(tail, acc)]
 end
